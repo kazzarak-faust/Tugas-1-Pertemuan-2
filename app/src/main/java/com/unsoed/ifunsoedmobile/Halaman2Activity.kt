e12ec03b66d33c6a6a68a9503ba9696256fff1a1
kazzarak-faust <muhammad.fausta@mhs.unsoed.ac.id>
2025-09-23 00:53:48 +0700

    Tugas 2 Pertemuan 3

--- app/src/main/java/com/unsoed/ifunsoedmobile/Halaman2Activity.kt
+++ app/src/main/java/com/unsoed/ifunsoedmobile/Halaman2Activity.kt
@@ -1,20 +1,71 @@
 package com.unsoed.ifunsoedmobile
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.net.toUri
+import com.unsoed.ifunsoedmobile.databinding.ActivityHalaman2Binding
+
 
 class Halaman2Activity : AppCompatActivity() {
+    private lateinit var binding: ActivityHalaman2Binding
+    private val latitude ="-7.429427"
+    private val longitude="109.338082"
+    private val gMapsUrl="http://maps.google.com/maps?q=loc:"
+    private val packageMaps="com.google.android.apps.maps"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_halaman2)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding=ActivityHalaman2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initLayout()
+        initListener()
+    }
+    private fun initLayout() {
+        binding.layoutLocation.let {
+            it.imgIcon.setImageResource(R.drawable.ic_location)
+            it.tvLayout.text = getString(R.string.alamat)
+        }
+        binding.layoutEmail.let {
+            it.imgIcon.setImageResource(R.drawable.ic_email)
+            it.tvLayout.text = getString(R.string.email)
+        }
+        binding.layoutIg.let {
+            it.imgIcon.setImageResource(R.drawable.ic_himpunan)
+            it.tvLayout.text = getString(R.string.ig_himpunan)
+        }
+        binding.layoutPhone.let {
+            it.imgIcon.setImageResource(R.drawable.ic_phone)
+            it.tvLayout.text = getString(R.string.telepon)
+        }
+    }
+    private fun initListener() {
+        binding.layoutLocation.root.setOnClickListener {
+            val gMapsIntentUri = "$gMapsUrl$latitude,$longitude".toUri()
+            val mapIntent = Intent(Intent.ACTION_VIEW, gMapsIntentUri)
+            startActivity(mapIntent.setPackage(packageMaps))
+        }
+        binding.layoutIg.root.setOnClickListener {
+            val igUri = getString(R.string.ig_himpunan).toUri()
+            val intent = Intent(Intent.ACTION_VIEW, igUri)
+            startActivity(intent)
+        }
+        binding.layoutEmail.root.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SENDTO).apply {
+                data = "mailto:${getString(R.string.email)}".toUri()
+            }
+            startActivity(intent)
+        }
+        binding.layoutPhone.root.setOnClickListener {
+            val intent = Intent(Intent.ACTION_DIAL).apply {
+                data = "tel:${getString(R.string.telepon)}".toUri()
+            }
+            startActivity(intent)
+        }
+        binding.btnBack.setOnClickListener {
+            finish()
         }
     }
 }
